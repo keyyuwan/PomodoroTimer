8accc90b87476a3c0cbfc0204bc72eecf5b20818
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-04-26 15:01:19 -0300

    TimerContext refactored :tada:

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
-import { useEffect, useState, } from 'react'
-
 import Header from './components/Header'
 import Timer from './components/Timer'
-import { TimerContext } from './contexts/TimerContext'
+import { TimerContextProvider } from './contexts/TimerContext'
 
 import './styles/global.css'
 
 function App() {
 
-  const [time, setTime] = useState(25 * 60)
-  const [isActive, setIsActive] = useState(false)
-  const [hasFinished, setHasFinished] = useState(false)
-  const [showTime, setShowTime] = useState(false)
-
-  function initiateTimer() {
-    setIsActive(true)
-    setShowTime(true)
-  }
-
-  let countdown;
-  function resetTimer() {
-    clearTimeout(countdown)
-    setIsActive(false)
-    setHasFinished(false)
-    setTime(25 * 60)
-    setShowTime(false)
-  }
-
-  function showNotification() {
-    new Audio('/notification.mp3').play()
-
-    if (Notification.permission === 'granted') {
-      new Notification('Pomodoro completado! 🎉', {
-        body: 'Uhul!'
-      })
-    }
-  }
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdown = setTimeout(() => { setTime(time - 1) }, 1000)
-    } else if (isActive && time === 0) {
-      showNotification()
-      setHasFinished(true)
-      setIsActive(false)
-    }
-  }, [isActive, time])
-
   return (
-    <TimerContext.Provider value={{ isActive, time, initiateTimer, resetTimer, hasFinished, showTime }}>
+    <TimerContextProvider>
       <div className="container">
         <Header />
         <Timer />
       </div>
-    </TimerContext.Provider>
+    </TimerContextProvider>
   );
 }
 
